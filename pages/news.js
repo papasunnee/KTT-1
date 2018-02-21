d8889bc1236fa950592ac9537258c6016e1b4897
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-02-21 11:26:19 +0100

    Fixed main videos carousel on KTT TV page

--- pages/news.js
+++ pages/news.js
@@ -14,7 +14,7 @@ class NewsPage extends Component {
   render(){
     return (
       <div>
-        <PageTitle breadcrumbs={breadcrumbs} title={"KTT Television"} isMediaPage>
+        <PageTitle breadcrumbs={breadcrumbs} title={"KTT Television"} isMediaPage isTelevisionPage>
           <HeroSection />
         </PageTitle>
         <Body />
