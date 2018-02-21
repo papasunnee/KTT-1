f11f32f9c858ee469c0edcc2bebafe5bcfdcd106
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-02-21 12:42:14 +0100

    programmatically remove string " State" from state names

--- components/common/StateList/StateList.js
+++ components/common/StateList/StateList.js
@@ -3,16 +3,13 @@ import gql from 'graphql-tag'
 import {Row, Col} from 'react-bootstrap'
 
 const StateList = props => {
-  const {stateMany} = props.data;
-  console.log(stateMany);
-  const states1 = [], states2 = [] ;
+  const stateMany = props.data.stateMany || [];
+  const states1 = [], states2 = [] , states = [];
   for (var i = 0; i < stateMany.length; i++) {
-    ((i+1)%2!==0) && states1.push(stateMany[i]);
-    ((i+1)%2===0) && states2.push(stateMany[i]);
+    states[i] = {name: (stateMany[i].name.replace(/ State/i,""))};
+    ((i+1)%2!==0) && states1.push(states[i]);
+    ((i+1)%2===0) && states2.push(states[i]);
   }
-  //console.log(states1);
-  /*console.log('stateprops');
-  console.log(stateMany);*/
   return (
   <div>
     <h6 className="text-bold" style={{textAlign: 'left'}}>States in Nigeria</h6>
