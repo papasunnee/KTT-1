949bd42d8bcd36b7e94327e6f45eaf72cc27eb15
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-03-02 10:07:29 +0100

    merge

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,12 @@ import EmpowermentZoneSection from '../components/HomePage/EmpowermentZoneSectio
 import TurakiMessage from '../components/HomePage/TurakiMessage/TurakiMessage'
 import ChoiceContent from '../components/HomePage/ChoiceContent/ChoiceContent'
 import GetInvolvedSection from '../components/HomePage/GetInvolvedSection/GetInvolvedSection'
-<<<<<<< HEAD
 //import EventsSection from '../components/HomePage/EventsSection/EventsSection'
 //import Endorsements from '../components/HomePage/Endorsements/Endorsements'
-import PollsSection from '../components/HomePage/PollsSection/PollsSection'
-=======
+//import PollsSection from '../components/HomePage/PollsSection/PollsSection'
 import EventsSection from '../components/HomePage/EventsSection/EventsSection'
 import Endorsements from '../components/HomePage/Endorsements/Endorsements'
 import PollResult from '../components/HomePage/PollsSection/PollResult'
->>>>>>> 2b6d5cb5330a9ad78f74372d22e4c9f03f8b59e0
 
 const firstTitle = 'Caption Text Here'
 const captionContent = 'Porem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente nulla temporibus voluptates ipsa, ducimus est, aliquid vel harum eligendi totam dignissimos suscipit obcaecati. Praesentium obcaecati ad accusamus voluptatum consequuntur, accusantium?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente nulla temporibus voluptates ipsa, ducimus est, aliquid vel harum eligendi totam dignissimos suscipit obcaecati. Praesentium obcaecati ad accusamus voluptatum'
--- pages/policy.js
+++ pages/policy.js
@@ -28,13 +28,10 @@ class AboutPage extends Component {
     const breadcrumbs = _breadcrumbs || defaultBreadcrumbs
     return (
       <div>
-<<<<<<< HEAD
         <HeroSection />
           <PageTitle title={title} breadcrumbs={breadcrumbs}/>
-=======
         <HeroSection imageSrc={imageSrc} />
         <PageTitle title={title} breadcrumbs={breadcrumbs}/>
->>>>>>> 2b6d5cb5330a9ad78f74372d22e4c9f03f8b59e0
         <TextBody />
       </div>
     )
