c773ba3457620554422503b55b95ecde8f64c918
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-03-02 10:05:47 +0100

    merge

--- pages/policy.js
+++ pages/policy.js
@@ -23,10 +23,18 @@ class AboutPage extends Component {
   }
   render(){
     const {_breadcrumbs, query} = this.props;
+    const title = query.policy || ''
+    const imageSrc = query.imageSrc || ''
+    const breadcrumbs = _breadcrumbs || defaultBreadcrumbs
     return (
       <div>
+<<<<<<< HEAD
         <HeroSection />
           <PageTitle title={title} breadcrumbs={breadcrumbs}/>
+=======
+        <HeroSection imageSrc={imageSrc} />
+        <PageTitle title={title} breadcrumbs={breadcrumbs}/>
+>>>>>>> 2b6d5cb5330a9ad78f74372d22e4c9f03f8b59e0
         <TextBody />
       </div>
     )
