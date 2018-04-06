579a60f8ae4940b5b88430bc6b96a472c7b1deb4
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-04-06 13:21:41 +0100

    moved media protal pages

---
+++ pages/media-portal/index.js
@@ -0,0 +1,30 @@
+import {Component} from 'react'
+//import Router from 'next/router'
+import Head from 'next/head'
+//import dynamic from 'next/dynamic'
+
+import withMedia from '../components/withMedia'
+import Home from '../components/MediaPortal/Index'
+//const Home = dynamic(import ('../components/MediaPortal/Index'), { ssr: false })
+
+/*const breadcrumbs = [
+  {title: 'Home', link:'/'},
+  {title: 'News', link:'/news'},
+  {title: 'Article', active: true},
+]*/
+
+class MediaAdminPage extends Component {  
+  render(){
+    const articles = this.props.articles || [];
+    return (
+      <div>
+        <Head>
+          <title>KTT4President | Media Portal</title>
+        </Head>
+        <Home />
+      </div>
+    )
+  }
+}
+
+export default withMedia(MediaAdminPage)
